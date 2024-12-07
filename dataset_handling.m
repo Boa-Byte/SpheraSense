880995fb9b255ad26160bcab7a367bfeaacd91bf
m.pytsepp <margaret.pytsepp@gmail.com>
2024-12-07 15:02:42 +0200

    - added running Ilastik commands in batches - added a progress bar for running Ilastik process

--- dataset_handling.m
+++ dataset_handling.m
@@ -1,65 +1,58 @@
 
 % Input = choose microscopy images folder
-% Output = master dataset "filenames_data_sorted"
+% Output = master dataset "filenames_data"
 
-folder_path = uigetdir('C:\Users\Kasutaja\Desktop\BSc_thesis');
-filenames = dir(fullfile(folder_path, '*.tif'));
-nr_images = numel(filenames);
-file_paths = cell(nr_images, 1);
+% Extract info from filenames
 
-pattern = '(?<WellID>\w+)_(?<ReadIndex>\d+)_(?<ChannelIndex>\d+)_(?<ZIndex>\w+)_(?<ChannelName>[\w\s]+)_(?<CycleIndex>\d+)';
-filenames_data = struct('FilePath', {}, 'WellID', {}, 'ReadIndex', {}, 'ChannelIndex', {}, 'ZIndex', {}, 'ZPlane', {}, 'ChannelName', {}, 'CycleIndex', {});
+function filenames_data = dataset_handling(folder_path)
 
-% Create main database (filenames_data)- Save path + Extract data from filenames
-
-for n = 1:nr_images
+    filenames = dir(fullfile(folder_path, '*.tif'));
+    nr_images = numel(filenames);
     
-    f = filenames(n).name;
-    file_data = regexp(f, pattern, 'names');
+    pattern = '(?<WellID>\w+)_(?<ReadIndex>\d+)_(?<ChannelIndex>\d+)_(?<ZIndex>\w+)_(?<ChannelName>[\w\s]+)_(?<CycleIndex>\d+)';
+    filenames_data = struct('FilePath', {}, 'WellID', {}, 'ReadIndex', {}, 'ChannelIndex', {}, 'ZIndex', {}, 'ZPlane', {}, 'ChannelName', {}, 'CycleIndex', {});
     
-    % New fields- FilePath and ZPlane
-    file_paths{n} = fullfile(folder_path, f);
-    file_data.FilePath = file_paths{n};
+    % Create main database
+    for n = 1:nr_images
+        
+        f = filenames(n).name;
+        file_data = regexp(f, pattern, 'names');
+        % To-do: Set aside files that don't match regex!
 
-    zindex_str = file_data.ZIndex;
-    z_plane = str2double(zindex_str(3));
-    file_data.ZPlane = z_plane;
+        % Add fields FilePath and ZPlane
+        file_data.FilePath = fullfile(folder_path, f);
+        file_data.ZPlane = str2double(file_data.ZIndex(3));
+    
+        filenames_data(n) = file_data; 
+    end
+    filenames_data = AN(filenames_data);
+end
 
-    filenames_data(n) = file_data;
+
+% Sort filenames_data alphanumerically by WellID
+function sorted = AN(filenames_data)
+
+    wellIDs = {filenames_data.WellID};
+    letters = cell(size(wellIDs));
+    numbers = zeros(size(wellIDs));
+
+    for i = 1:length(wellIDs)
+        wellID = wellIDs{i};
+        letters{i} = wellID(1);
+        numbers(i) = str2double(wellID(2:end));
+    end
+
+    [~, sorted_idx] = sortrows([cell2mat(letters'), numbers']);  % Sort by letters, then by numbers
+    sorted = filenames_data(sorted_idx);                         % Reorder filenames_data based on sorted indices
 
 end
 
-% Sort filenames_data by WellID
-wellIDs = {filenames_data.WellID};
-
-% Loop to separate letters and numbers
-% + Capture unique IDs (for well options display)
-
-letters = cell(size(wellIDs));
-numbers = zeros(size(wellIDs));
-
-for i = 1:nr_images
-    wellID = wellIDs{i};
-    letters{i} = wellID(1);
-    numbers(i) = str2double(wellID(2:end));
-end
-
-[~, sorted_idx] = sortrows([cell2mat(letters'), numbers']); % Sort by letters, then by numbers
-filenames_data_sorted = filenames_data(sorted_idx);         % Reorder filenames_data based on sorted indice
-
-wellIDs = {filenames_data_sorted.WellID};
-uniqueIDs = unique(wellIDs, 'stable');
-
-%zplane_values = unique(cell2mat({filenames_data_sorted.ZPlane}));
-
-%% 
-
-value = 'A3'; % User chosen well, show only relevant data
-wellIDs = {filenames_data_sorted.WellID};
-well_selected = filenames_data_sorted(strcmp(wellIDs, value));
-
-z_value = 2;
-%zplanes = [well_selected.ZPlane];
-%z_selected = well_selected(zplanes == z_value);
-
-z_selected = well_selected([well_selected.ZPlane] == z_value);
+% value = 'A3'; % User chosen well, show only relevant data
+% wellIDs = {filenames_data_sorted.WellID};
+% well_selected = filenames_data_sorted(strcmp(wellIDs, value));
+% 
+% z_value = 2;
+% %zplanes = [well_selected.ZPlane];
+% %z_selected = well_selected(zplanes == z_value);
+% 
+% z_selected = well_selected([well_selected.ZPlane] == z_value);
---
+++ run_ilastik.m
@@ -0,0 +1,151 @@
+% README:
+
+% run_ilastik(imfolder, ilastik_proj_path)
+% runs ilastik headless
+% imfolder = path to folder of microscopy images
+% ilastik_proj_path = path of Ilastik ML model (.ilp file)
+
+% gen_cmds_ilastik(imfolder, ilastik_proj_path)
+% generates commands for all .tif files in img folder
+
+
+function run_ilastik(imfolder, ilastik_proj_path, progress)
+
+    cd(imfolder);
+    cmds = gen_cmds_ilastik(imfolder, ilastik_proj_path);
+
+    n = numel(dir(fullfile(imfolder, '*.tif')));
+    n_done = 0;
+    
+    for i = 1:numel(cmds)
+        
+        system(cmds(i).command); % Run a batch of commands  
+        
+        while n_done < n
+            n_done = numel(dir(fullfile(imfolder, '*.h5')));
+            progress.Value = n_done / n;
+            progress.Message = sprintf('Processing image %d of %d...', n_done, n);
+        end        
+    end
+    
+    % move resulting h5 files to a Probabilities folder
+    h5_files = dir(fullfile(imfolder, '*.h5'));
+    dest_path = [fullfile(imfolder), '_Probabilities'];
+    mkdir(dest_path);
+    
+    for j = 1:length(h5_files)
+        movefile(h5_files(j).name, dest_path);
+    end
+
+end
+
+
+function cmds = gen_cmds_ilastik(imfolder, ilastik_proj_path)
+
+    % Find any version of Ilastik.exe from user computer
+    basePath = 'C:\Program Files\';
+    wildcardPath = dir(fullfile(basePath, 'ilastik*'));
+    if ~isempty(wildcardPath)
+        ilastik_path = fullfile(basePath, wildcardPath(1).name, 'ilastik.exe'); % get full path
+    else
+        error('Ilastik not found among Program Files.');
+    end
+    
+    % Generate batches of terminal commands < 8191 char (Windows default limit)
+    images = dir(fullfile(imfolder,'*.tif'));
+    cmds = struct('command', {});
+    batch_nr = 0;
+    i = 1;
+    
+    while i <= numel(images)
+    
+        cmd = sprintf('"%s" --headless --project="%s" "%s"', ...
+            ilastik_path, ...
+            ilastik_proj_path, ...
+            images(i).name);
+        
+        cmd_length = length(cmd);
+        i = i+1;
+        
+        % Keep adding images if within the character limit
+        while i <= numel(images)
+            
+            img_str = sprintf(' "%s"', images(i).name);
+            
+            if cmd_length + length(img_str) >= 8191
+                break;
+            end
+    
+            cmd = strcat(cmd, img_str);
+            cmd_length = length(cmd); % Update command length
+            i = i+1;                  % Move to the next image
+        
+        end
+    
+        % Finalize batch
+        batch_nr = batch_nr + 1;
+        cmds(batch_nr).command = cmd;
+    end
+end
+
+
+% function trackprogress(imfolder)
+% 
+%     % Checking nr files vs nr nr files done
+%     progress = uiprogressdlg(app.UIFigure, 'Title', 'Processing', 'Message', 'Starting...');
+%     n = length(dir(imfolder));
+%     n_done = 0;
+%     
+%     while n_done < n
+%         progress.Value = n_done / n;
+%         progress.Message = sprintf('Processing image %d of %d...', n_done, n);
+%         n_done = numel(dir(fullfile(imfolder, '*.h5')));
+%         pause(0.5); % Pause to avoid excessive CPU usage
+%     end
+%     
+%     close(progress);
+% end
+
+
+% logFile = 'process_log.txt';
+% errorLog = 'error_log.txt';
+% 
+% logs = fopen(logFile, 'a');
+% errors = fopen(errorLog, 'a');
+% 
+% if logs == -1
+%     error('Failed to open process_log file.');
+% end
+% if errors == -1
+%     error('Failed to open error_log file.');
+% end
+% 
+% cleanup = onCleanup(@() fclose('all')); % close all open files when the script ends
+% 
+% % Execute commands, catch errors
+% try
+%     for i = 1:numel(cmds)
+%         fprintf(logs, 'Analyzing image %d/%d: %s', i, numel(cmds), images(i).name);
+%         status = system(cmds(i).command); % Run command
+%         
+%         if status == 0
+%             fprintf(logs, ' - Success.\n');
+%             % BUG - ütleb success kui on clear error, A1_bad.tif file on
+%             % saast
+%         else
+%             fprintf(logs, 'Image %d failed with status %d.\n', i, status);
+%             fprintf(errors, 'Image %d failed: %s\n', i, cmds(i).command);
+%         end
+%     end
+% 
+% catch ME
+%     fprintf(logs, 'Script interrupted with error: %s\n', ME.message);
+%     fprintf(errors, 'Script interrupted with error: %s\n', ME.message);
+%     fprintf(errors, 'Error ID: %s\n', ME.identifier);
+%     % save failed things to run again?
+%     % critical errors - stop the loop?
+% 
+% end
+% 
+% fclose(logs);
+% fclose(errors);
